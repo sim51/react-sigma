d476b944e661afe250613126cbb88ee0b6dd4028
sim51 <github@bsimard.com>
2024-07-25 11:24:42 +0000

    Deploying to gh-pages from @ sim51/react-sigma@6ecad831940cadf00d6df114e372dd06d2933519 🚀

--- storybook/assets/EdgeCurve.stories-THSSdlqu.js
+++
@@ -1,9 +0,0 @@
-import{M as s,s as t}from"./MultiDirectedGraph-CXuwkxVr.js";import"./react-sigma.min-CrKgMPKo.js";import"./index-Dl6G-zuu.js";import"./useRandom-AQQFxtCG.js";const u={component:s,id:"edge-curve",title:"Sigma programs"},r={name:"Edge curved with arrows",args:{},parameters:{storySource:{source:t}}};var e,a,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  name: "Edge curved with arrows",
-  args: {},
-  parameters: {
-    storySource: {
-      source
-    }
-  }
-}`,...(o=(a=r.parameters)==null?void 0:a.docs)==null?void 0:o.source}}};const d=["Default"];export{r as Default,d as __namedExportsOrder,u as default};
--- storybook/assets/MultiDirectedGraph.stories-CeyinXpY.js
+++
@@ -1,9 +0,0 @@
-import{M as o,s}from"./MultiDirectedGraph-CXuwkxVr.js";import"./react-sigma.min-CrKgMPKo.js";import"./index-Dl6G-zuu.js";import"./useRandom-AQQFxtCG.js";const p={component:o,id:"multi-directed",title:"Examples"},e={name:"Instantiate a MultiDirectedGraph on SigmaContainer",args:{},parameters:{storySource:{source:s}}};var r,t,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  name: "Instantiate a MultiDirectedGraph on SigmaContainer",
-  args: {},
-  parameters: {
-    storySource: {
-      source
-    }
-  }
-}`,...(a=(t=e.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const u=["Default"];export{e as Default,u as __namedExportsOrder,p as default};
